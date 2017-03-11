c1e2d032483d0ef3593d5b0f2b5340b555a18fb5
Lucas Gimenez <lucas07gimenez@gmail.com>
2017-03-11 20:12:15 -0300

    Uploaded with the script

--- script.sh
+++ script.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 
-#Script para poder subir de forma automática archivos a un repositorio remoto en GitHub utilizando comandos de Git.
+####################################################################################################################
+#Script para poder subir de forma automática archivos a un repositorio remoto en GitHub utilizando comandos de Git.#
+####################################################################################################################
+
+
+#Para poder hacer las cosas más faciles ingresamos la ruta de los archivos que vamos a subir a GitHub.
+
+#echo "RUTA DONDE SE ENCUENTRAN LOS ARCHIVOS QUE SERÁN SUBIDOS AL REPOSITORIO REMOTO (ejemplo: /home/lucas/repo/LPC1769_UNM ):"
+#read ruta
+
+#cd ruta
 
 #Iniciamos o re-inicializamos git.
 
@@ -8,6 +18,7 @@ git init
 
 #Vemos el estado de nuestro repositorio local, en este preciso momento.
 
+echo "ESTADO DEL WORKSPACE"
 git status
 
 #Agregamos los archivos modificados, de nuestro espacio de trabajo, que están listos para confirmar.
@@ -16,6 +27,7 @@ git add -A
 
 #Volvemos a ver el estado de nuestro repo local. En este punto deberíamos ver en "verde" lo que anteriormente estaba en "rojo"
 
+echo "ESTADO DEL WORKSPACE"
 git status
 
 #Confirmamos los archivos, enviando un mensaje de commit al repositorio.
@@ -24,7 +36,7 @@ git commit -m "Uploaded with the script"
 
 #Creamos una variable donde guardaremos la 'url' del repositorio remoto que ingrese el usuario por pantalla.
 
-echo "Ingresá la 'url' del repositorio remoto:"
+echo "INGRESÁ LA 'URL' DEL REPOSITORIO REMOTO:" 
 read url
 
 #Nos conectamos (linkeamos) al repositorio remoto ingresado, donde vamos a subir nuestros archivos.
@@ -37,10 +49,11 @@ git remote -v
 
 #Si todo está en orden, procedemos a subir los archivos.
 
-echo "Subiendo archivos al repositorio remoto" #Mostramos por pantalla que la carga de los archivos se inició.
+echo "SUBIENDO ARCHIVOS AL REPOSITORIO REMOTO" #Mostramos por pantalla que la carga de los archivos se inició.
+echo "INGRESAR USUARIO Y CONTRASEÑA DE TU CUENTA DE GitHub" #Necesario para Git.
 
 git push origin master
 
-echo "Carga terminada" #Mostramos por pantalla que la carga de los archivos finalizó.
+echo "CARGA TERMINADA" #Mostramos por pantalla que la carga de los archivos finalizó.
 
 
